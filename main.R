bff14c93d35f16ef4ff6500908275a5739af9bde
florentdaburon <florent.daburon@unil.ch>
2023-10-13 16:13:29 +0200

    Data formating

--- main.R
+++ main.R
@@ -15,9 +15,26 @@ data <- data[,-3]
 data <- data[-1,]
 # Make the first line the name of the columns
 colnames(data) <- data[1,]
-#change format of table to long table
-library(reshape2)
-data <- melt(data, id.vars = "Country Name", variable.name = "Year", value.name = "CPI")
+#exctracte column 2 and name it weight
+weight <- data[,2]
+#drop the first line
+weight <- weight[-1,]
+#remove first line and secdond column from data
+data <- data[,-2]
+#change the first index of the table to "Year"
+data[1,1] <- "Year"
+#Reshape the data so the years which is the first line are in one column
+data <- t(data)
+#make the first line the name of the columns
+colnames(data) <- data[1,]
+#remove the first line
+data <- data[-1,]
+#format the data to numeric
+data <- as.numeric(data)
+#format the first column of date to display a date from the excel way of counting
+data[,1] <- as.Date(as.numeric(data[,1]), origin = "1899-12-30")
+
+
 
 #ARIMA MODEL
 install.packages("forecast")
