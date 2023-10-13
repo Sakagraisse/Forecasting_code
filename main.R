736e0a4c42e94a62d23c64f233a62f38f792f079
florentdaburon <florent.daburon@unil.ch>
2023-10-13 14:54:33 +0200

    Initial commit

---
+++ main.R
@@ -0,0 +1,10 @@
+#import data in excell format from the first table only
+install.packages("readxl")
+library(readxl)
+data <- read_excel("CPI_2020.xlsx", sheet = 1, col_names = TRUE, skip = 1)
+
+#Remove the first 5 columns
+data <- data[,9:479]
+#Remove the
+#Extract the second column
+data2 <- data[,2]
