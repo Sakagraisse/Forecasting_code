b0e98ff05a84ed4f30cdaacad801d5112bd7838d
abigailvasquez <cw.abigailvasquez@gmail.com>
2023-11-08 17:36:18 +0100

    Fixes done

--- main.R
+++ main.R
@@ -95,59 +95,78 @@ library(tseries)
 
 #check for stationarity of the data
 adf.test(data4$Rent)
-#take the first difference of the data
-#data4$Rent_diff <- c(NA, diff(data4$Rent, differences = 1))
-#check for stationarity of the data
-#adf.test(data4$Rent_diff)
-
-
-# Difference the data to achieve stationarity
-#data2$rent_diff <- c(NA, diff(data2$rent, differences = 1))
-# Create the differenced rent time series excluding the first NA observation
-#rent_diff <- diff(data2$rent, differences = 1)
-# Run your Augmented Dickey-Fuller test again on the differenced rent
-#adf.test(rent_diff)
-# Difference the mortgage rate series and remove the first NA value
-#mortgage_rate_diff <- diff(data2$mortgage.rate, differences = 1)
-#mortgage_diff <- diff(data2$mortgage.rate, differences = 1)
 
 #ARIMA model for rent  with mortgage rate as exogenous variable
 fit <- auto.arima(data3$Rent, xreg = data3$mortagage.rate, seasonal=FALSE, approximation=FALSE, trace=TRUE)
 summary(fit)
 checkresiduals(fit)
 
-
 ## Forecast of the mortgage rate
-# Create a time series object of the mortgage rate data
-#mortgage_rates_ts <- ts(data2$mortgage.rate, start=c(2008, 9), frequency=12)
+#check for stationarity of the data
 adf.test(data3$mortagage.rate)
-#mortgage_diff <- diff(mortgage_rates_ts, differences = 1)
-#adf.test(mortgage_diff)
 
 # Fit an ARMA model to the historical mortgage rate data
 fit2 <- auto.arima(data3$mortagage.rate, seasonal=FALSE, stepwise=TRUE, approximation=FALSE)
 checkresiduals (fit2)
-
 # Use the fitted ARMA model to forecast future mortgage rates
 future_mortgage_rate_forecast <- forecast(fit2, h=12)
-
 # The forecast object contains point forecasts, lower and upper confidence intervals
 print(future_mortgage_rate_forecast)
-
 # Plot the forecast to visualize
 plot(future_mortgage_rate_forecast)
-
 # Extract the point forecasts of the future mortgage rates
 future_mortgage_rate_values <- future_mortgage_rate_forecast$mean
-
 # Forecasted mortgage rate values as the exogenous variable in the rent forecast
 rent_forecast <- forecast(fit, xreg=future_mortgage_rate_values, h=12)
 # Plot the forecast of rent
 plot(rent_forecast)
-
 # Print the forecasted rent values
 print(rent_forecast)
 
 #Performance evaluation
 library(ggplot2)
+#out-of-sample evaluation
+# Plot the forecasted rent values
+plot(rent_forecast)
 
+# Set the number of periods to forecast
+n <- 12
+# Split the data into training and test sets
+train_end <- length(data4$Rent) - n
+train_set <- data4$Rent[1:train_end]
+test_set <- data4$Rent[(train_end + 1):length(data4$Rent)]
+
+# Fit the ARIMA model on the training set
+fit <- auto.arima(train_set)
+
+# Forecast n periods ahead
+rent_forecast <- forecast(fit, h = n)
+
+# The forecast object contains point forecasts, upper and lower confidence intervals
+# Extract the point forecasts
+forecast_values <- rent_forecast$mean
+
+# Compare the forecast to the actual values in the test set
+comparison <- data.frame(Forecast = forecast_values, Actual = test_set)
+
+# Calculate accuracy measures
+accuracy_measures <- accuracy(forecast_values, test_set)
+
+# Print the comparison and accuracy measures
+print(comparison)
+print(accuracy_measures)
+
+# This assumes that your test_set is already a time series object with proper time attributes
+forecast_series <- ts(forecast_values, start=start(test_set), frequency=frequency(test_set))
+
+# Create a combined data frame for plotting
+actual_df <- data.frame(Time = time(test_set), Rent = as.numeric(test_set), Type = "Actual")
+forecast_df <- data.frame(Time = time(forecast_series), Rent = as.numeric(forecast_series), Type = "Forecasted")
+all_data <- rbind(actual_df, forecast_df)
+
+# Plot using ggplot2
+ggplot(all_data, aes(x = Time, y = Rent, color = Type)) +
+  geom_line() +
+  labs(title = "Actual vs Forecasted Rent", x = "Time", y = "Rent") +
+  scale_color_manual(values = c("Actual" = "blue", "Forecasted" = "red")) +
+  theme_minimal()
