163d77c8ab1789b8b5fe7388a1c416d05beba377
abigailvasquez <cw.abigailvasquez@gmail.com>
2023-11-08 01:15:00 +0100

    Fix data

--- main.R
+++ main.R
@@ -53,23 +53,7 @@ data <- data.frame(lapply(data, as.numeric))
 #format the first column of date to display a date from the excel way of counting using openxlsx package
 data$Year <- as.Date(as.numeric(data$Year), origin = "1899-12-30")
 
-## data cleaning end ##
-
-
-
-#Plots ----------
-# plot the column "Heating oil" against the column "Year"
-plot(data$Year, data$`Heating.oil`, type = "l", col = "red", xlab = "Year", ylab = "Heating oil", main = "Heating oil against the year")
-# plot the column "Fuel" against the column "Year"
-plot(data$Year, data$Fuel, type = "l", col = "blue", xlab = "Year", ylab = "Fuel", main = "Fuel against the year")
-# plot the column “Total.1" against the column "Year"
-plot(data$Year, data$Total.1, type = "l", col = "green", xlab = "Year", ylab = "Total.1", main = "Total.1 against the year")
-
-#### mortgage rate construction data monthly------
-
-#import excel file with mortgage rate
-data2 <- read_excel("/Users/abigailvasquez/Desktop/Econ Forcasting/CPI_Forecast/mortgage_rate_c.xlsx", sheet = 1, col_names = TRUE, skip = 1)
-
+data2 <- read_excel("mortgage_rate_c.xlsx", sheet = 1, col_names = TRUE)
 #rename first column "mortgage rate"
 colnames(data2)[1] <- "mortgage.rate"
 #rename second column "date"
@@ -82,106 +66,10 @@ data2 <- data2[order(data2$date),]
 data2 <- data2[rep(row.names(data2), each = 3), ]
 
 #create dates
-data2$dates <- seq(as.Date("2008-09-10"), by = "month", length.out = nrow(data2))
+data2$dates <- seq(as.Date("2008-09-01"), by = "month", length.out = nrow(data2))
 
 #delete date column
 data2 <- data2[,-2]
 
 #keep data2 until line 156
-data2 <- data2[1:156,]
-
-###### construction mortgage + rent for ARIMA with mortgage rate lag 4 for exogenous variable
-#create data for "rent" from data starting at lign 309 to 462 (to merge with mortgage rate)
-data4 <- data[309:464,]
-data2$rent <- data4$Rent
-
-
-###### essai ARIMA
-#arima model for rent
-install.packages("forecast")
-library(forecast)
-
-fit3 <- auto.arima(data2$rent, approximation=FALSE, trace=TRUE, ic="aic")
-
-#forecast for rent
-forecast_results <- forecast(fit3, h=10)
-plot(forecast_results)
-
-#arima  model for rent  with mortgage rate  as exogenous variable
-fit4 <- auto.arima(data2$rent, xreg = data2$mortgage.rate, approximation=FALSE, trace=TRUE, ic="aic")
-forecast_results2 <- forecast(fit3, h=10)
-plot(forecast_results2)
-checkresiduals (fit4)
-
-#arima  model for rent  with mortgage rate  as exogenous variable with lag 4 for mortgage rata
-fit5 <- auto.arima(data2$rent, xreg = data2$mortgage.rate, approximation=FALSE, trace=TRUE, ic="aic")
-forecast_results3 <- forecast(fit3, h=20)
-plot(forecast_results3)
-checkresiduals (fit5)
-
-#quarterly Arima model
-fit6 <- auto.arima(data2$rent, xreg = data2$mortgage.rate, approximation=FALSE, trace=TRUE, ic="aic", seasonal = TRUE)
-# Residual analysis
-checkresiduals(fit6)
-forecast_results4 <- forecast(fit3, h=20)
-plot(forecast_results4)
-# Plot forecast against actual data
-plot(forecast_results4, main = "ARIMA Forecast")
-summary(forecast_results4)
-
-
-#### mortgage rate construction data monthly
-
-#import excel file with mortgage rate
-data2 <- read_excel("mortgage_rate_c.xlsx", sheet = 1, col_names = TRUE, skip = 1)
-
-#rename first column "mortgage rate"
-colnames(data2)[1] <- "mortgage.rate"
-#rename second column "date"
-colnames(data2)[2] <- "date"
-
-#order date
-data2 <- data2[order(data2$date),]
-
-#create monthly data for mortgage rate in duplicating the data 3 times to have monthly value instead of quarterly
-data2 <- data2[rep(row.names(data2), each = 3), ]
-
-#create dates
-data2$dates <- seq(as.Date("2008-09-10"), by = "month", length.out = nrow(data2))
-
-#delete date column
-data2 <- data2[,-2]
-
-#keep data2 until line 156
-data2 <- data2[1:156,]
-
-
-###### construction mortgage + rent for ARIMA with mortgage rate lag 4 for exogenous variable
-#create data for "rent" from data starting at lign 309 to 462 (to merge with mortgage rate)
-data4 <- data[309:464,]
-data2$rent <- data4$Rent
-
-###### essai ARIMA
-#arima model for rent
-fit3 <- auto.arima(data2$rent, seasonal = FALSE)
-
-#forecast for rent
-forecast_results <- forecast(fit3, h=10)
-plot(forecast_results)
-
-#arima  model for rent  with mortgage rate  as exogenous variable
-fit4 <- auto.arima(data2$rent, xreg = data2$mortgage.rate, seasonal = FALSE)
-forecast_results2 <- forecast(fit3, h=10)
-plot(forecast_results2)
-
-#arima  model for rent  with mortgage rate  as exogenous variable with lag 4 for mortgage rata
-fit5 <- auto.arima(data2$rent, xreg = data2$mortgage.rate, seasonal = FALSE, max.p = 5, max.q = 5, max.d = 5, max.order = 10, max.D = 5, max.P = 5, max.Q = 5)
-forecast_results3 <- forecast(fit3, h=20)
-plot(forecast_results3)
-
-#quarterly Arima model
-fit6 <- auto.arima(data2$rent, xreg = data2$mortgage.rate, seasonal = TRUE, max.p = 5, max.q = 5, max.d = 5, max.order = 10, max.D = 5, max.P = 5, max.Q = 5)
-checkresiduals(fit6)
-forecast_results4 <- forecast(fit3, h=20)
-plot(forecast_results3
-     ##linear combination for adding our forecasts
+data2 <- data2[1:181,]
