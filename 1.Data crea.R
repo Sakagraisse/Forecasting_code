a53990b92c0afcb86ca8433a76ee1095038803d9
dabur <florent.daburon@unil.ch>
2023-12-11 17:04:18 +0100

    Ok for out et in test for ARMAx

--- 1.Data crea.R
+++ 1.Data crea.R
@@ -77,55 +77,54 @@ save(weight, file = "weight.RData")
 
 ### mortgage
 
-#import excel file with mortgage rate
+#import excel file with mortgage rate anc create quarterly data
 Mortgage <- read_excel("mortgage_rate_c.xlsx", sheet = 1, col_names = TRUE)
 #rename first column "mortgage rate"
 colnames(Mortgage)[1] <- "mortgage_rate"
 #rename second column "date"
 colnames(Mortgage)[2] <- "date"
-#create a dataframe with only dates generated monthly from 2008-03-01 to 2023-09-01
-dates <- seq(as.Date("2008-07-01"), as.Date("2023-09-01"), by = "month")
-indicator_monthly <- ts(dates, start=c(2008,7), frequency=12)
-Mortgage <- ts(Mortgage$mortgage_rate, frequency = 4, start = c(2008,3))
-Mortgage_q <- Mortgage
-monthly_series <- td(Mortgage ~ 1, to = "monthly", method = "denton-cholette")
-mort_monthly_numeric <- as.numeric(monthly_series[[1]])
-#add the dates to the dataframe
-Mortgage <- data.frame(dates, mort_monthly_numeric)
-# remove uneeded asset in memory
-rm(dates, indicator_monthly, monthly_series, mort_monthly_numeric)
+Mortgage <- Mortgage[order(Mortgage$date, decreasing = FALSE),]
+length(Mortgage$mortgage_rate)
+
+# Répéter les taux trimestriels pour chaque mois dans l'année
+taux_mensuels <- rep(Mortgage$mortgage_rate, each = 3)
+mortgage_rate_monthly <- ts(taux_mensuels, start = c(2008,9), frequency = 12)
+
 
 ### Rent
 rental <- CPIs
 #remove data before 2008-07-01
-CPIs_trunk <- rental[rental$Year >= as.Date("2008-07-01"),]
+CPIs_trunk <- rental[rental$Year >= as.Date("2008-09-01"),]
+#keep only rent
+#rental <- as.numeric(CPIs_trunk$Housing.rental.1)
 #keep only rent
 rental <- CPIs_trunk$Housing.rental.1
 #convert to ts
-rental <- ts(rental, frequency = 12, start = c(2008,3))
+rental <- ts(rental, frequency = 12, start = c(2008,9))
 #convert to quarterly data
 quarterly_series <- aggregate(rental, nfrequency = 4, FUN = mean)
-rent_q <- quarterly_series
-# convert to monthly data
-monthly_series <- td(quarterly_series ~ 1, to = "monthly", method = "additive")
-rent_monthly_numeric <- as.numeric(monthly_series[[1]])
-dates <- seq(as.Date("2008-07-01"), as.Date("2023-09-01"), by = "month")
-indicator_monthly <- ts(dates, start=c(2008,7), frequency=12)
-#add the dates to the dataframe
-Rent <- data.frame(dates, rent_monthly_numeric)
 
-# remove uneeded asset in memory
-rm(dates, indicator_monthly, monthly_series, rent_monthly_numeric, rental, quarterly_series)
+rental <- as.numeric(rental)
+#create data frame with rent and mortgage monthly
+length(mortgage_rate_monthly)
+length(rental)
+length(CPIs_trunk$Year)
+mortgage_rate_monthly <- mortgage_rate_monthly[c(1:181)]
+Rent_fore <- data.frame(CPIs_trunk$Year, rental, mortgage_rate_monthly)
 
-# add mortgage and rent to CPIs_trunk
-CPIs_trunk$Rent <- Rent$rent_monthly_numeric
-CPIs_trunk$Mortgage <- Mortgage$mort_monthly_numeric
-#save CPIs_trunk
-save(CPIs_trunk, file = "CPIs_trunk.RData")
+#save the data
+save(Rent_fore, file = "Rent_fore.RData")
 
-Date_q <- seq(as.Date("2008-07-01"), as.Date("2023-09-01"), by = "quarter")
-Mortgage_q <- as.numeric(Mortgage_q)
-rent_q <- as.numeric(rent_q)
-Rent_fore_q <- data.frame(Date_q,Mortgage_q,rent_q)
+## create quarterly data for rental by averaging the monthly data
+length(Mortgage$mortgage_rate)
+length(quarterly_series)
 
+mortgage_rate_q <- Mortgage$mortgage_rate
+mortgage_rate_q <- mortgage_rate_q[c(1:60)]
+quarterly_series <- as.numeric(quarterly_series)
+
+Rent_fore_q <- data.frame(mortgage_rate_q, quarterly_series)
+#create quarterly dates colum
+Rent_fore_q$Date <- seq(as.Date("2008-09-01"), as.Date("2023-06-01"), by = "3 month")
+#save the data
 save(Rent_fore_q, file = "Rent_fore_q.RData")
--- 6 ARMAX Rent Mortgage.R
+++ 6 ARMAX Rent Mortgage.R
@@ -26,27 +26,202 @@ library(lubridate)
 library(tempdisagg)
 library(openxlsx)
 
+################### code quartely ############################
 
+
+rm(list = ls())
 #load data : CPIs.RData
-load("CPIs_trunk.RData")
-CPIs <- CPIs_trunk
-rm(CPIs_trunk)
+load("Rent_fore_q.RData")
+CPIs <- Rent_fore_q
+rm(Rent_fore_q)
 #
+#rename the columns
+colnames(CPIs) <- c( "Mortgage", "Rent", "Date")
+#change to numeric
+CPIs$Rent <- as.numeric(CPIs$Rent)
+CPIs$Mortgage <- as.numeric(CPIs$Mortgage)
 
 # #calculate the Ren and Mortgage.rate over the year inflation rate
-CPIs$Rent <- log(CPIs$Rent/lag(CPIs$Rent,12))
-CPIs$Mortgage <- log(CPIs$Mortgage/lag(CPIs$Mortgage,12))
+Rent <- log(CPIs$Rent/lag(CPIs$Rent,4))
+Mortgage <- log(CPIs$Mortgage/lag(CPIs$Mortgage,4))
+#Rent <- ts(CPIs$Rent, start = c(2008,9), frequency = )
+#Mortgage <- ts(CPIs$Mortgage, start = c(2008,9), frequency = 12)
 #plot the inflation rate
-plot(CPIs$Year, CPIs$Rent, type = "l", col = "red", xlab = "Year", ylab = "Inflation rate", main = "Inflation rate of rent")
-plot(CPIs$Year, CPIs$Mortgage, type = "l", col = "red", xlab = "Year", ylab = "Inflation rate", main = "Inflation rate of mortgage")
+#plot(Rent, type = "l", col = "red", xlab = "Year", ylab = "Inflation rate", main = "Inflation rate of rent")
+#plot(Mortgage, type = "l", col = "red", xlab = "Year", ylab = "Inflation rate", main = "Inflation rate of mortgage")
 
-#auto arimaX fit CPIs$Rent using CPIs$Mortgage as exogenous variable
-fit <- auto.arima(CPIs$Rent, xreg = CPIs$Mortgage, seasonal = FALSE, approximation = FALSE, trace=TRUE)
+length(Rent)
+length(Mortgage)
+Mortgage <- ts(Mortgage, start = c(2009,9), frequency = 4)
+Rent <- ts(Rent, start = c(2009,9), frequency = 4)
+Rent <- na.omit(Rent)
+Mortgage <- na.omit(Mortgage)
+Mortgage <- ts(Mortgage, start = c(2009,9), frequency = 4)
+Rent <- ts(Rent, start = c(2009,9), frequency = 4)
 
-# #reduce data to remove NAs introduced by the lag
-CPIs <- CPIs[13:nrow(CPIs),]
 
-#manually check the best model by finding p,d,q in an ARIMA(p,d,q) model
-#check for d with dickey fuller test
-adf.test(CPIs$Rent, alternative = "stationary", k = trunc((length(CPIs$Rent)-1)^(1/3)))
-## NOT stationnary
+# Create the fourth lag of this difference
+Mortgage <- as.numeric(Mortgage)
+mortgage.rate.lag4 <- lag(Mortgage, 4)
+
+Mortgage <- ts(mortgage.rate.lag4, start = c(2009,9), frequency = 4)
+ #Remove the NA values that come from lagging
+Mortgage <- na.omit(Mortgage)
+Mortgage <- ts(Mortgage, start = c(2010,9), frequency = 4)
+
+Rent <- as.numeric(Rent)
+Rent <- Rent[-c(1:4)]
+Rent <- ts(Rent, start = c(2010,9), frequency = 4)
+
+length(Rent)
+length(Mortgage)
+
+fit <- auto.arima(Rent, xreg = Mortgage, seasonal = FALSE, approximation = FALSE, trace=TRUE)
+
+fit2 <- auto.arima(Mortgage, seasonal = TRUE, approximation = FALSE, trace=TRUE)
+forecast_mortgage <- forecast(fit2, h = 12)
+plot(forecast_mortgage)
+#forecast_mortgage <- ts(forecast_mortgage$mean, start = c(2025,1), frequency = 4)
+plot(forecast_mortgage)
+
+#forecast CPIs$Rent using CPIs$Mortgage as exogenous variable
+forecast_rent <- forecast(fit, xreg = forecast_mortgage$mean, h = 12)
+plot(forecast_rent)
+
+
+######
+# In sample tests
+######
+
+#calculate the in sample residuals
+in_sample_residuals <- fit$residuals
+#calculate the in sample RMSE
+in_sample_RMSE <- sqrt(mean(in_sample_residuals^2))
+#calculate the in sample MAE
+in_sample_MAE <- mean(abs(in_sample_residuals))
+
+base_stat <- data.frame(in_sample_RMSE, in_sample_MAE)
+rm(in_sample_MAE, in_sample_RMSE)
+# Ljung Box-Q Test
+Ljung <- Box.test(in_sample_residuals, lag = 10, type = "Ljung-Box", fitdf = 3)
+# White Test
+Pierce <- Box.test(in_sample_residuals, lag = 10, type = "Box-Pierce", fitdf = 3)
+# jarque bera test
+Jarques <- jarque.bera.test(in_sample_residuals)
+# White Test
+White <- white_test(fit)
+
+in_sample_tests <- data.frame(Ljung$p.value, White$p_value, Jarques$p.value,Pierce$p.value)
+rm(Ljung, Pierce, Jarques, White)
+
+
+
+
+
+
+
+
+### out of sample forecast for benchmark model
+
+the_Rent <- ts(Rent, start = c(2009,9), frequency = 4)
+the_Mort <- ts(Mortgage, start = c(2009,9), frequency = 4)
+out_of_sample <- data.frame(matrix(ncol = 1, nrow = 12))
+mean_of_fit <- data.frame(matrix(ncol = 1, nrow = 12))
+end <- length(the_Rent)
+end <- end - 12
+plot(the_Rent)
+
+#iterate from line 36 to the en of CPIs
+for (i in 13:end){
+    temporary <- the_Rent[1:i-1]
+    temporary <- ts(temporary, start = c(2009,9), frequency = 4)
+    temporary_m <- the_Mort[1:i-1]
+    temporary_m <- ts(temporary_m, start = c(2009,9), frequency = 4)
+    end_year <- end(temporary)[1]
+    end_month <- end(temporary)[2]
+    #fit arima model on the first i-1 observations
+    fit <- arima(temporary, order = c(1,0,0), xreg = temporary_m)
+    fit_m <- arima(temporary_m, order = c(1,0,1))
+    forecast_fit <- forecast(fit_m, h = 12)
+    #forecast the i-th observation
+    fore <- predict(fit, newxreg=forecast_fit$mean, h = 12)
+    print <- ts(fore$pred, start = c(end_year, end_month + 1 ), frequency = 4)
+    lines(print, col="red")
+    #calculate the out of sample forecast
+    to_save <- (as.numeric(fore$pred) - the_Mort[i:i+12])^2
+    out_of_sample <- data.frame(out_of_sample, to_save)
+    mean_of_fit <- data.frame(mean_of_fit, fore$pred)
+
+}
+
+#dev.off()
+#remove first column
+Squared <- out_of_sample[,-1]
+MSFE_by_time <- colMeans(Squared[1,] , na.rm = TRUE)
+for (i in 2:12){
+    MSFE_by_time <- rbind(MSFE_by_time, colMeans(Squared[1:i,] , na.rm = TRUE))
+}
+MSFE_Total <- rowMeans(MSFE_by_time, na.rm = TRUE)
+#rm(end, end_month, end_year, fore, i, mean_of_fit, out_of_sample, print, temporary, to_save)
+
+
+### out of sample forecast for benchmark model
+
+the_Rent_b <- ts(Rent, start = c(2009,9), frequency = 4)
+the_Mort_b <- ts(Mortgage, start = c(2009,9), frequency = 4)
+out_of_sample_b <- data.frame(matrix(ncol = 1, nrow = 12))
+mean_of_fit_b <- data.frame(matrix(ncol = 1, nrow = 12))
+end_b <- length(the_Rent_b)
+end_b <- end_b - 12
+plot(the_Rent_b)
+
+#iterate from line 36 to the en of CPIs
+for (i in 13:end_b){
+    temporary <- the_Rent_b[1:i-1]
+    temporary <- ts(temporary, start = c(2009,9), frequency = 4)
+    temporary_m <- the_Mort_b[1:i-1]
+    temporary_m <- ts(temporary_m, start = c(2009,9), frequency = 4)
+    end_year <- end(temporary)[1]
+    end_month <- end(temporary)[2]
+    #fit arima model on the first i-1 observations
+    fit <- arima(temporary, order = c(1,0,0), xreg = temporary_m)
+    fit_m <- arima(temporary_m, order = c(1,0,1))
+    forecast_fit <- forecast(fit_m, h = 12)
+    #forecast the i-th observation
+    fore <- predict(fit, newxreg=forecast_fit$mean, h = 12)
+    print <- ts(fore$pred, start = c(end_year, end_month + 1 ), frequency = 4)
+    lines(print, col="red")
+    #calculate the out of sample forecast
+    to_save_b <- (as.numeric(fore$pred) - the_Mort[i:i+12])^2
+    out_of_sample_b <- data.frame(out_of_sample_b, to_save_b)
+    mean_of_fit_b <- data.frame(mean_of_fit_b, fore$pred)
+
+}
+
+#dev.off()
+#remove first column
+Squared_b <- out_of_sample_b[,-1]
+MSFE_by_time_b <- colMeans(Squared_b[1,] , na.rm = TRUE)
+for (i in 2:12){
+    MSFE_by_time_b <- rbind(MSFE_by_time_b, colMeans(Squared_b[1:i,] , na.rm = TRUE))
+}
+MSFE_Total_b <- rowMeans(MSFE_by_time_b, na.rm = TRUE)
+#rm(end, end_month, end_year, fore, i, mean_of_fit, out_of_sample, print, temporary, to_save)
+
+
+
+#Create MSFE by Time
+Squared_b <- out_of_sample_b[,-1]
+
+MSFE_by_time_b <- colMeans(Squared_b[1,] , na.rm = TRUE)
+for (i in 2:36){
+    MSFE_by_time_b <- rbind(MSFE_by_time_b, colMeans(Squared_b[1:i,] , na.rm = TRUE))
+}
+MSFE_Total_b <- rowMeans(MSFE_by_time_b, na.rm = TRUE)
+
+rm(end_b, end_month, end_year, fore, i, mean_of_fit_b, out_of_sample_b, print, temporary, to_save)
+
+MSFE_pred_by_time <- 1 - (MSFE_Total/MSFE_Total_b)
+MSFE_pred <- 1 - (MSFE_Total[36]/MSFE_Total_b[36])
+
+plot(MSFE_pred_by_time, type = "l", col = "red", xlab = "Time", ylab = "MSFE", main = "MSFE by time")
